7e18e7e3c3691c38759cdc15afd907f940a58e1c
Sebastian M. Alvarez <sebastianmalvarez@gmail.com>
2013-04-21 09:06:56 -0300

    Updating

--- legalia/questions/views.py
+++ legalia/questions/views.py
@@ -1,7 +1,21 @@
 # Create your views here.
 from django.shortcuts import render
+from questions.models import Question
 
 def index(request):
 	print request.user.is_authenticated()
-	context = {'auth': request.user.is_authenticated(), 'user': request.user}
+	context = {
+		'auth': request.user.is_authenticated(),
+		'user': request.user,
+		'questions': Question.objects.all()[:15]
+	}
+	return render(request, 'questions/index.html', context)
+
+def one(request, id):
+	print request.user.is_authenticated()
+	context = {
+		'auth': request.user.is_authenticated(),
+		'user': request.user,
+		'questions': Question.objects.all()[:15]
+	}
 	return render(request, 'questions/index.html', context)
